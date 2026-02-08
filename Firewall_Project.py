7dbc8e61725f54f97d77d226ff8e5c2bc7839ec7
JonathanKho2005 <c5042227@newcastle.ac.uk>
2026-02-08 23:29:25 +0000

    Create generate_random_ip() and check_firewall_rules(ip,rules),Add a for loop in main()

--- Firewall_Project.py
+++ Firewall_Project.py
@@ -1,9 +1,26 @@
 import random
+from ipaddress import ip_address
+
+
 def main():
     firewall_rules = {
         "10.130.131.4":"block",
-        "172.16.7.70":"block",
+        "10.130.131.1":"block",
     }
 
-if __name__() = '__main__()':
+    for x in range(10):
+        ip = generate_random_ip()
+        action = check_firewall_rules(ip, firewall_rules)
+        print(f"IP: {ip}, Action: {action}")
+
+def generate_random_ip():
+    return f"10.130.131.{random.randint(1,10)}"
+
+def check_firewall_rules(ip, rules):
+    for rule_ip, action in rules.items():
+        if ip == rule_ip:
+            return action
+    return "allow"
+
+if __name__ == '__main__':
     main()
