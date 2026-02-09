d214dcb4401d413fcacc1599081c658cd7d434af
JonathanKho2005 <c5042227@newcastle.ac.uk>
2026-02-09 17:54:48 +0000

    Remove get_current_datetime and remains the now variable. Create the write_log(log) functino

--- Firewall_Project3.py
+++ Firewall_Project3.py
@@ -1,12 +1,9 @@
-# from scapy.all import sniff, IP
-# from Firewall_Project2 import handle_packet, check_firewall_rules
 import datetime
 
-def get_current_datetime():
-    now = datetime.datetime.now().strftime("%m/%d/%Y %I:%M:%S %p")
-    print(now)
+now = datetime.datetime.now().strftime("%m/%d/%Y %I:%M:%S %p")
 
-if __name__ == '__main__':
-    get_current_datetime()
+def write_log(log):
+    with open("firewall_log.txt", "a") as log_file:
+        log_file.write(f"{log}\n")
 
 
