235298060a19d8e43ff56a253ca90f6302df1254
JonathanKho2005 <c5042227@newcastle.ac.uk>
2026-02-09 18:02:22 +0000

    Add variable timestamp and log_message and write_log function under the handle_packet(packet)

--- Firewall_Project2.py
+++ Firewall_Project2.py
@@ -1,5 +1,6 @@
 from scapy.all import sniff
 from scapy.all import IP
+from Firewall_Project3 import write_log,now
 
 firewall_rules = {
     "20.189.173.18":"block",
@@ -13,9 +14,14 @@ def handle_packet(packet):
         action = check_firewall_rules(src, firewall_rules)
         print(f"Source IP: {src} -> {action}")
 
+        #Create timestamp and log_message
+        timestamp = now()
+        log_message = f"{timestamp} | {src} | {action}"
+
+        #Write logging into text file records
+        write_log(log_message)
 
 def check_firewall_rules(ip,rules):
     return rules.get(ip,"allow")
-
 
 sniff(prn=handle_packet)
